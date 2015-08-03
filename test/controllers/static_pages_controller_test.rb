940b8f2d18dd73ea2039fb798bc0d6da525d4a6a
Morgan Howell <morgan.howell95@gmail.com>
2015-08-02 17:34:14 -0700

    adding styles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Morgan Howell"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Morgan Howell" 
+    assert_select "title", "#{@base_title}" 
   end
 
   test "should get portfolio" do
     get :portfolio
     assert_response :success
-    assert_select "title", "Portfolio | Morgan Howell" 
+    assert_select "title", "Portfolio | #{@base_title}" 
   end
 
   test "should get bio" do
   	get :bio
   	assert_response :success
-  	assert_select "title", "Bio | Morgan Howell" 
+  	assert_select "title", "Bio | #{@base_title}" 
   end
 
 end
