65c0c65314accbb072a766cd97d5372654856485
Morgan Howell <morgan.howell95@gmail.com>
2015-08-17 23:19:22 -0700

    adding user signups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      redirect_to @user #also written as redirect_to user_url(@user)
+      flash[:success] = "Welcome to my app #{@user.name}"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'bio' => 'static_pages#bio'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
